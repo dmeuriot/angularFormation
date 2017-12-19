cdb2fd07c3e846e649a9126ded0d23d5aaf32e5a
dmeuriot <dmeuriot.dylanmeuriot@gmail.com>
2017-12-19 13:53:21 +0100

    ngfor ngif card

---
+++ my-app/src/app/items/components/item/item.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Item } from '../../../shared/interfaces/item.model';
+import { State } from '../../../shared/enmus/state.enum';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.scss']
+})
+export class ItemComponent implements OnInit {
+  state = State;
+  @Input('item') item: Item;
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
--- my-app/src/app/items/items.module.ts
+++ my-app/src/app/items/items.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ItemComponent } from './components/item/item.component';
+import { ListItemsComponent } from './components/list-items/list-items.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  exports: [
+    ItemComponent,
+    ListItemsComponent
+  ],
+  declarations: [
+    ItemComponent,
+    ListItemsComponent
+  ]
 })
 export class ItemsModule { }
